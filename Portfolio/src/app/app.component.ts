5b85c51bda6cf4862e9227e1bf7c0ac62a79568e
hernanborre <borre.hernan@gmail.com>
2022-11-10 17:09:26 -0300

    add autolink activation depending on scroll and viewport

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -2,15 +2,21 @@
 import { Component } from '@angular/core';
 import Typewriter from 't-writer.js'
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 
 export class AppComponent implements OnInit{
   title = 'Portfolio';
 
+
   ngOnInit(): void {
   }
+
+
 }
+
+
--- Portfolio/src/app/home/home.component.ts
+++ Portfolio/src/app/home/home.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SetEverythingOffExcept } from '../services/NavHelpers'
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+@HostListener('window:scroll', ['$event'])
+  public onViewportScroll(e) {
+    let child = e.srcElement.children[0]
+    let scrollToTop = e.srcElement.children[0].scrollTop
+    if(scrollToTop >= 0 && scrollToTop <= 708 ) {
+      SetEverythingOffExcept('hero-nav-id', document)
+    } else if(scrollToTop >= 709 && scrollToTop <= 1254 ) {
+      SetEverythingOffExcept('about-nav-id', document)
+    }else if(scrollToTop >= 1255 && scrollToTop <= 1670 ) {
+      SetEverythingOffExcept('skills-nav-id', document)
+    } else if(scrollToTop >= 1660 && scrollToTop <= 2700 ) {
+      SetEverythingOffExcept('resume-nav-id', document)
+    } else if(scrollToTop >= 2701 && scrollToTop <= 2973 ) {
+      SetEverythingOffExcept('portfolio-nav-id', document)
+    } else if(scrollToTop >= 2974 && scrollToTop <= 4000 ) {
+      SetEverythingOffExcept('contact-nav-id', document)
+    }
+    //console.log(bottom)
+    console.log(child.scrollHeight)
+    console.log(child.scrollTop)
+    console.log(child.clientHeight)
+  }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }

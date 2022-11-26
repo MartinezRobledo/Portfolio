a747fd7d6d560e047110e88068a657259b2e6894
MartinezRobledo <adrianfmart@gmail.com>
2022-11-26 07:43:05 -0300

    implement reactive form in about section

--- Portfolio/src/app/about-me/about-me.component.ts
+++ Portfolio/src/app/about-me/about-me.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { Persona } from '../Models/Persona';
 import { DataService } from '../services/data.service';
@@ -13,11 +13,12 @@ export class AboutMeComponent implements OnInit {
 
   adrian:Persona;
   edit:boolean;
+  error:boolean = false;
   edad:number;
-  updateForm:FormGroup;
-  fecha = '2015-05-02'
 
-  constructor(private dataService:DataService) { 
+  formGroup:FormGroup;
+
+  constructor(private dataService:DataService, private formBuilder:FormBuilder) { 
     this.adrian = Object.assign({}, this.dataService.persona);
     this.edit = this.dataService.login;
     this.edad = this.CalculateAge(this.adrian.birthdate);
@@ -36,11 +37,72 @@ export class AboutMeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.formGroup = this.formBuilder.group({
+      email: [this.adrian.email, [Validators.email, Validators.required]],
+      birthplace: [this.adrian.birthplace, [Validators.minLength(3), Validators.maxLength(100), Validators.required]],
+      birthdate: [this.adrian.birthdate],
+      location: [this.adrian.location, [Validators.minLength(3), Validators.maxLength(100), Validators.required]],
+      level: [this.adrian.level, [Validators.minLength(3), Validators.maxLength(20), Validators.required]],
+      ocupation: [this.adrian.ocupation, [Validators.minLength(3), Validators.maxLength(30), Validators.required]],
+      phone: [this.adrian.phone, [Validators.maxLength(10), Validators.minLength(10), Validators.required]],
+      name: [this.adrian.name],
+      lastname: [this.adrian.lastname],
+      titulo: [this.adrian.titulo]
+    });
   }
 
   changeField(event:any){
     this.adrian[event.target.name] = "";
     this.adrian[event.target.name] += event.target.value;
+  }
+
+  onSubmit(){
+    this.adrian = this.formGroup.value;
+    this.dataService.updatePersona(this.adrian);
+  }
+
+  public getError(controlName: string):string{
+    let error = '';
+    const control = this.formGroup.get(controlName);
+    if (control.touched && control.errors != null) {
+      error = JSON.stringify(control.errors);
+    }
+    return this.typeError(error);
+  }
+
+  typeError(error:string):string{
+    const errorList:string[] = ['required', 'maxlength', 'minlength', 'email'];
+    let valor:string;
+    errorList.forEach((e) => {
+      if(error.includes(e)){
+        valor = e
+      }
+    })
+    return this.showError(valor);
+  }
+
+  showError(error:string):string{
+    let message = '';
+
+    switch(error){
+      case 'required':
+        message = 'El campo es requerido';
+        break;
+      case 'maxlength':
+        message = 'El valor ingresado supera el máximo permitido';
+        break;
+      case 'minlength':
+        message = 'El valor ingresado es demasiado corto';
+        break;
+      case 'email':
+        message = 'El email es inválido. Por favor ingrese un formato válido'
+        break;
+    }
+    return message;
   }
 
   SendDataonChange(event: any) {
@@ -49,10 +111,6 @@ export class AboutMeComponent implements OnInit {
 
   restoreData(){
     this.adrian = Object.assign({},this.dataService.persona);
-  }
-
-  saveChanges(){
-    this.dataService.updatePersona(this.adrian)
   }
 
 }
